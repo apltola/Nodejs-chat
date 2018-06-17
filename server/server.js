2acfaecfa2e9e2858308b4d23f8f71a824c6e790
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-06-17 15:26:45 +0300

    adding socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('NEW USER CONNECTED')
+
+  socket.on('disconnect', () => {
+    console.log('HEY SOMEBODY DISCONNECTED!')
+  })
+})
+
 app.use(express.static(publicPath));
-console.log(publicPath);
-
-
 const PORT = process.env.PORT || 3222;
-app.listen(PORT);
-console.log(`THE SERVER LIVES!! ${PORT}`);
+server.listen(PORT, () => console.log(`THE SERVER LIVES!! ${PORT}`));
