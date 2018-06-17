a8f72e733eee1655613248a472a016d3fce5164d
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-06-17 18:25:33 +0300

    adding form to user interface

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,18 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  let divi = jQuery('<div class="testi"></div>');
+  divi.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(divi);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'anon',
+    text: jQuery('[name=message]').val()
+  }, function() {
+    
+  });
 });
